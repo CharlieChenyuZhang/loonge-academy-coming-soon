4131cdb305ef17163f44845635424c8364a39380
Lijia Yu <kathy-ylj@hotmail.com>
2024-07-01 11:16:49 -0500

    Add email and LinkedIn to coming soon page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaEnvelope, FaLinkedin } from "react-icons/fa";
+import { MdOutlineMailOutline } from "react-icons/md";
 import "./App.css";
 import logo from "./loong.academy_logo.png";
 
@@ -7,12 +9,31 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} alt="Logo" className="App-logo" />
+        <h2 className="academy">Loong.Academy</h2>
         <h1 className="fade-in">Coming Soon</h1>
         <h2 className="fade-in">Empowering Minds, Expanding Horizons</h2>
         <p className="fade-in">
           Learn Beyond Boundaries with AI-Driven Education
         </p>
       </header>
+      <div className="links fade-in">
+        <a
+          href="mailto:contact@chenyuzhang.com"
+          className="icon-link"
+          aria-label="Email"
+        >
+          <FaEnvelope />
+        </a>
+        <a
+          href="https://www.linkedin.com/company/loong-academy/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon-link"
+          aria-label="LinkedIn"
+        >
+          <FaLinkedin />
+        </a>
+      </div>
     </div>
   );
 }
